a6ce5b0d1fe070db26bd4bda36e6cce8c6f6de59
hoon9732 <70583704+hoon9732@users.noreply.github.com>
2025-08-14 11:04:45 +0900

    Create app_settings.py

---
+++ 00. Coding/02. Python/gui/modules/app_settings.py
@@ -0,0 +1,17 @@
+class Settings():
+    # APP SETTINGS
+    ENABLE_CUSTOM_TITLE_BAR = True
+    MENU_WIDTH = 240
+    LEFT_BOX_WIDTH = 240
+    RIGHT_BOX_WIDTH = 240
+    TIME_ANIMATION = 500
+
+    # BTNS LEFT AND RIGHT BOX COLORS
+    BTN_LEFT_BOX_COLOR = "background-color: rgb(58, 49, 44);"
+    BTN_RIGHT_BOX_COLOR = "background-color: #ffc679;"
+
+    # MENU SELECTED STYLESHEET
+    MENU_SELECTED_STYLESHEET = """
+    border-left: 22px solid qlineargradient(spread:pad, x1:0.034, y1:0, x2:0.216, y2:0, stop:0.499 rgba(255, 198, 121, 255), stop:0.5 rgba(255, 170, 85, 0));
+    background-color: rgb(52, 44, 40);
+    """
